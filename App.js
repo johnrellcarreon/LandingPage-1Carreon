e8dd8a962ebc9a0f191611adcf8d3d4ad7fd3f5e
johnrellcarreon <103201116+johnrellcarreon@users.noreply.github.com>
2022-06-01 18:25:45 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+         Welcome to Carreon's Landing Page from BSIT2A
+        </p>
+     </header>
+    </div>     
+  );
+}
+
+export default App;
